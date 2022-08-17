15b5afa92eb7a2259ece32c4f954147d27c62663
John <jsaita48@gmail.com>
2022-08-17 08:20:49 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function receivesAFunction(callback){
+  return callback();
+}
+function returnsANamedFunction(){
+  var callback =function (){
+    return function name(){
+      return 'anonymous';
+    }
+  }
+
+  return receivesAFunction(callback);
+}
+function returnsAnAnonymousFunction(){
+  return function (){
+    console.log('anonymous')
+  }
+
+}
